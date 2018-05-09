69b0c132ef8c83c9a0b47dbf9e2924e783e6feac
Henry Wang <skymatrix@126.com>
2018-05-09 17:40:33 +0800

    Add comments to show more run methods

--- run.py
+++ run.py
@@ -5,8 +5,10 @@ from hobby.testcase import module1_inputs, module2_combine
 executor.run(conf.windows, module1_inputs, module2_combine)
 
 """
-# You can run your specified modules or test cases, just append them as the next parameters.
-executor.run(conf.windows, module1_inputs.TestCase01_BasicVerify)
-executor.run(conf.windows, module1_inputs.TestCase01_BasicVerify, module2_combine)
 
+You can run your specified modules or test cases or their combinations, just append them as the second 
+or next parameters.
 """
+# executor.run(conf.windows, module1_inputs.TestCase01_UserInformation)
+# executor.run(conf.windows, module1_inputs.TestCase02_Gender, module2_combine)
+
